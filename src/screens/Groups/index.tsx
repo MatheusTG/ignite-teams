ff7a20b18aa416e1ae3d1fcc0954e728d9818361
Matheus <matheusteodorogarcia@gmail.com>
2024-07-11 11:56:42 -0300

    HighLight component

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,11 +1,13 @@
 import { Text } from 'react-native';
 import { Container } from './styles';
 import { Header } from '@components/Header';
+import HighLight from '@components/HighLight';
 
 export function Groups() {
   return (
     <Container>
-      <Header  />
+      <Header />
+      <HighLight title="Turmas" subtitle="Jogue com a sua turma" />
     </Container>
   );
 }
