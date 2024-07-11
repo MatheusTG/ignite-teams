05a04c5cdb4a30a199862372a9230f30c01d8580
Matheus <matheusteodorogarcia@gmail.com>
2024-07-11 12:51:30 -0300

    Group listing

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -2,14 +2,27 @@ import { GroupCard } from '@components/GroupCard';
 import { Container } from './styles';
 import { Header } from '@components/Header';
 import HighLight from '@components/HighLight';
+import { useState } from 'react';
+import { FlatList } from 'react-native';
+import { ListEmpty } from '@components/ListEmpty';
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>([]);
+
   return (
     <Container>
       <Header />
       <HighLight title="Turmas" subtitle="Jogue com a sua turma" />
 
-      <GroupCard title="Galera do Ignite" />
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => <GroupCard title={item} />}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        ListEmptyComponent={
+          <ListEmpty message="Que tal cadastrar a primeira turma?" />
+        }
+      />
     </Container>
   );
 }
